86ab693fecce55b95514b48bfc52c73ccd28df08
JOHNSON LIU <jrliu@us.ibm.com>
2018-11-07 15:42:01 -0500

    remove

--- server.js
+++ server.js
@@ -4,39 +4,7 @@ var request = require("request");
 var handleRequest = function (req, res) {
   console.log('Received request for URL: ' + req.url);
   res.writeHead(200);
-  // response.end('Hello World!' + request.url);
-
-  if (req.url == "/mc") {
-    console.log('went to mc');
-    var options = {
-      method: 'GET',
-      url: 'https://tile-management-console.garage-catalog-prod.us-south.containers.appdomain.cloud/',
-      headers:
-      {
-      }
-    };
-
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-      res.end(body);
-    });
-  } else if (req.url == "/sb") {
-    console.log('went to service broker');
-    var options = {
-      method: 'GET',
-      url: 'https://tile-service-broker.garage-catalog-prod.us-south.containers.appdomain.cloud/',
-      headers:
-      {
-      }
-    };
-
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-      res.end(body);
-    });
-  }
+  
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
